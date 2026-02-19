3116460ee58c1915680e77f55a0303239a7d23ae
Taras Shcherban <sherbantaras535@gmail.com>
2026-02-19 17:42:59 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,32 +2,64 @@
 #include <WiFi.h>
 #include <WebServer.h>
 
-
 #define LED_PIN 2
 
 WebServer server(80);
 
+const char* html = R"""(
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Esp32 control</title>
+</head>
+<body>
+    <div class="control-container">
+    <h1>Esp32 Led Blink</h1>
+    <button onclick="toggleLED('on')">Turn ON</button>
+    <button onclick="toggleLED('off')">Turn OFF</button>
+    </div>
+
+    <script>
+        const toggleLED = (state) => {
+            fetch('/' + state)
+                .then(response => response.text())
+                .then(data => console.log(data))
+                .catch(err => console.error(err));
+        }
+    </script>
+</body>
+</html>
+)"""; 
+
+const char* ap_ssid = "ESP32-AP";
+const char* ap_password = "12345678";
+
+void handleRoot() {
+  server.send(200, "text/html", html);
+}
+
 void setup() {
-  pinMode(LED_PIN, OUTPUT);
-  
   Serial.begin(115200);
+  pinMode(LED_PIN, OUTPUT);
 
   WiFi.mode(WIFI_AP);
-  WiFi.softAP("ESP32-Blink", "12345678");
-  Serial.printf("IP: %s\n", WiFi.softAPIP().toString().c_str());
+  WiFi.softAP(ap_ssid, ap_password);      
+  Serial.print("AP IP: ");
+  Serial.println(WiFi.softAPIP());
 
-  server.on("/", []() {
-    server.send(200, "text/html", "<html><body><h1>ESP32 LED</h1></body><html>");
-  });
+  server.on("/", handleRoot);
 
-    server.on("/on", []() {
+  server.on("/on", []() {
     digitalWrite(LED_PIN, HIGH);
     server.send(200, "text/html", "LED is ON");
   });
 
   server.on("/off", []() {
     digitalWrite(LED_PIN, LOW);
-    server.send(200, "text/html", "LED if OFF");
+    server.send(200, "text/html", "LED is OFF");
   });
 
   server.begin();
@@ -35,9 +67,4 @@ void setup() {
 
 void loop() {
   server.handleClient();
-
-  digitalWrite(LED_PIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_PIN, LOW);
-  delay(1000);
 }
